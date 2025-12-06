25896547a890482d0dd5c84479ec896015ef00ef
Ancarrenv <acarrenov@unal.edu.co>
2025-12-06 06:20:35 -0500

    clase 6 ajustes

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,1 +1,14 @@
+# Ejemplo para casos de uso
+# python3 -m homework data/input data output
 
+import sys
+
+
+def parse_args():
+    input_folder = sys.argv[1]
+    output_folder = sys.argv[2]
+    return input_folder, output_folder
+
+
+def main():
+    args = parse_args()
